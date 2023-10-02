5b58b8bccfbe623f6d6b4408735e84b22f84490d
Dedios <dediosprogramming@gmail.com>
2023-10-02 16:09:08 -0400

    variable creation

---
+++ gpa-calculator.cs
@@ -0,0 +1,25 @@
+string studentName = "Sophia Johnson";
+string course1Name = "English 101";
+string course2Name = "Algebra 101";
+string course3Name = "Biology 101";
+string course4Name = "Computer Science I";
+string course5Name = "Psychology 101";
+
+int course1Credit = 3;
+int course2Credit = 3;
+int course3Credit = 4;
+int course4Credit = 4;
+int course5Credit = 3;
+/* OUTPUT: SHOULD LOOK LIKE
+
+
+Student: Sophia Johnson
+
+Course          Grade   Credit Hours	
+English 101         4       3
+Algebra 101         3       3
+Biology 101         3       4
+Computer Science I  3       4
+Psychology 101      4       3
+
+Final GPA:          3.35 */ 
